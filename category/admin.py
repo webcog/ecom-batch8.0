39fc1a588ac06e0d2817d91f97b929bdbe5a9715
webcog <wajahatmurtaza144@gmail.com>
2024-01-29 20:03:22 +0500

    29-1-2024

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from category.models import Category, Wajahat
+
 # Register your models here.
+
+admin.site.register(Wajahat)
+admin.site.register(Category)
--- category/models.py
+++ category/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Categories"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category_name
+    
+
+class Wajahat(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = "Wajahat Murtaza"
+        verbose_name_plural = "Wajahat Murtaza"
