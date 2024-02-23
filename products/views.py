90e2c03b218a7bda182ab945d9cb9866529924a3
webcog <wajahatmurtaza144@gmail.com>
2024-02-23 21:08:33 +0500

    23-02-2024

--- products/views.py
+++ products/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from products.models import Product
 from category.models import Category
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
 
 def index(request):
     category = Category.objects.all()
+    products = Product.objects.all()
     context = {
-        "category":category
+        "category":category,
+        "products":products,
     }
 
     return render(request, 'index.html',context)
 
-
+# @login_required(login_url="login")
 def shop(request):
     products = Product.objects.all()
     context = {
@@ -20,6 +24,8 @@ def shop(request):
     }
     return render(request, 'shop.html', context)
 
+
+# @login_required(login_url="login")
 def detail(request, slug):
     product = Product.objects.get(slug=slug)
     context = {
