06379ddd1b6264ddcbe05a4a2509020bcce401a3
webcog <wajahatmurtaza144@gmail.com>
2024-03-25 20:31:34 +0500

    update

--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,41 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from orders.models import Order, OrderProduct
+from cart.models import Cart, CartItems
+from orders.forms import OrderForm
 # Create your views here.
 
 @login_required(login_url='login')
 def checkout(request):
-    return render(request, 'orders/checkout.html')
+    cart = Cart.objects.filter(user=request.user, is_paid=False).first()
+    cart_items = CartItems.objects.filter(cart=cart)
+
+    subtotal = sum(item.total_price() for item in cart_items )
+    # 2% shipping from subtotal     
+    tax = 0.02 * subtotal
+    shipping = 180
+    total = subtotal + shipping + tax
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user 
+            order.order_total = total
+            order.tax = tax
+            order.shipping_charges = shipping
+            order.save()
+            
+
+
+
+
+
+    context = {
+        'cart_items':cart_items,
+        'subtotal':subtotal,
+        'shipping':shipping,
+        'total':total,
+        'tax':tax,
+    }
+    return render(request, 'orders/checkout.html', context)
